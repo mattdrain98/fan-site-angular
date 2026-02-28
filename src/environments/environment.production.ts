f9ab56da53e52bf595dea164e73e42b605eae91e
mattdrain98 <matthewdrain98@gmail.com>
2026-02-28 18:44:53 -0500

    update production path

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://fanwebsiteapi20260228180319-hsfjg3dtchcgebe9.canadacentral-01.azurewebsites.net/api'  // add whatever variables your app needs
+};
